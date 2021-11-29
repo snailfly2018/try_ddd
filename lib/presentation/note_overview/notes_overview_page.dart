35239ef513ee083dd4f653a4ab8266e5628e83e2
coder_asus <taobo2018@gmail.com>
2021-11-29 10:59:02 -0800

    19 notes overview 页面

--- lib/presentation/note_overview/notes_overview_page.dart
+++ lib/presentation/note_overview/notes_overview_page.dart
@@ -1,8 +1,12 @@
+import 'package:another_flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:try_ddd/application/auth/auth_bloc.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:try_ddd/application/notes/note_actor/note_actor_bloc.dart';
+import 'package:try_ddd/application/notes/note_watcher/note_watcher_bloc.dart';
+import '../../injection.dart';
 import '../router/router.gr.dart';
 
 class NotesOverviewPage extends StatelessWidget {
@@ -10,31 +14,59 @@ class NotesOverviewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<AuthBloc, AuthState>(
-      listener: (context, state) {
-        state.maybeMap(
-          unauthenticated: (_) => context.replaceRoute(const SignInRoute()),
-          orElse: () {
-            //听到登入成功并不做什么
-          },
-        );
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Notes'),
-          leading: IconButton(
-            onPressed: () {
-              context.read<AuthBloc>().add(const AuthEvent.signedOut());
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<NoteWatcherBloc>(
+          create: (context) => getIt<NoteWatcherBloc>()
+            ..add(const NoteWatcherEvent.watchAllStarted()),
+        ),
+        BlocProvider<NoteActorBloc>(
+            create: (context) => getIt<NoteActorBloc>()),
+      ],
+      child: MultiBlocListener(
+        listeners: [
+          BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) {
+              state.maybeMap(
+                  unauthenticated: (_) =>
+                      context.replaceRoute(const SignInRoute()),
+                  orElse: () {});
             },
-            icon: const Icon(Icons.exit_to_app),
           ),
-        ),
-        body: const Center(
-          child: Text('note body'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
+          BlocListener<NoteActorBloc, NoteActorState>(
+              listener: (context, state) {
+            state.maybeMap(
+                deleteFailure: (state) {
+                  FlushbarHelper.createError(
+                    duration: const Duration(seconds: 5),
+                    message: state.noteFailure.map(
+                      unexpected: (_) =>
+                          'Unexpected error occured while deleting, please contact support',
+                      insufficientPermission: (_) => 'Insufficient Permissions',
+                      unableToUpdate: (_) => 'Impossible Error',
+                    ),
+                  ).show(context);
+                },
+                orElse: () {});
+          }),
+        ],
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Notes'),
+            leading: IconButton(
+              onPressed: () {
+                context.read<AuthBloc>().add(const AuthEvent.signedOut());
+              },
+              icon: const Icon(Icons.exit_to_app),
+            ),
+          ),
+          body: const Center(
+            child: Text('note body'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
         ),
       ),
     );
