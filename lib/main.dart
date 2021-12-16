92da8b5f35775acc06afdce803f1531ab1419f31
coder_asus <taobo2018@gmail.com>
2021-12-16 12:25:25 -0800

    web 版

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:try_ddd/presentation/core/app_widget.dart';
@@ -9,6 +12,20 @@ Future<void> main() async {
   // Bloc.observer = MyBlocObserver();
   WidgetsFlutterBinding.ensureInitialized();
   configureInjection(Environment.prod); //注入
-  await Firebase.initializeApp();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyBC7Muw1zsYqCBQb91kI6fNB7cjY0kYPyw",
+          authDomain: "ddd-note-app-70a22.firebaseapp.com",
+          projectId: "ddd-note-app-70a22",
+          storageBucket: "ddd-note-app-70a22.appspot.com",
+          messagingSenderId: "423320686303",
+          appId: "1:423320686303:web:7772e418c1bdfccdb7636a"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(AppWidget());
 }
